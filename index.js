aaa579e206d9a15f7013f90f1f4ce42b56279f2d
Derek Ekins <derek@spathi.com>
2015-02-12 08:43:03 +0000

    cache compiled pogo using a hash of its input as the key return the cached javascript when the same input is supplied

--- index.js
+++ index.js
@@ -2,11 +2,35 @@ var pogo = require('pogo');
 var fs = require('fs');
 var through = require('through');
 var convert = require('convert-source-map');
+var checksum = require('checksum');
+var mkdirp = require('mkdirp');
+var cachePath = process.cwd() + "/.pogoCache/";
+
+mkdirp.sync(cachePath)
+
+function getCached(hash) {
+  if (fs.existsSync(cachePath + hash)) {
+    return fs.readFileSync(cachePath + hash, 'utf8');
+  }
+}
+
+function writeCache(hash, contents){
+  fs.writeFile(cachePath + hash, contents);
+}
 
 function compile(file, data) {
     var compiled = '';
     try {
-        compiled = pogo.compile(fs.readFileSync(file, 'utf8'));
+        var fileContents = fs.readFileSync(file, 'utf8');
+        var hash = checksum(fileContents);
+        cached = getCached(hash);
+        if (cached) {
+            compiled = cached;
+        }
+        else {
+            compiled = pogo.compile(fileContents);
+            writeCache(hash, compiled);
+        }
     }
     catch (e) {
         throw new Error("PogoScript compilation failed for '" + file + "'\n" + e.toString())
